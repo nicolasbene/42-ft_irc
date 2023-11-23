fef7b905d607b9e87679bf467d6b8024c8a9dba2
Jean-baptiste GAUTIER <jgautier@paul-f5Ar1s2.clusters.42paris.fr>
2023-11-23 18:17:04 +0100

    changement du nick en cas de doublon fait mais pas username car pb

--- src/Commands/Pass.cpp
+++ src/Commands/Pass.cpp
@@ -3,14 +3,14 @@
 
 void Server::PassAuthentification(Message msg, int fd)
 {
-    if (msg.getParameters().empty())
+  if (msg.getParameters().empty())
 		sendServerRpl(fd, ERR_NEEDMOREPARAMS(users[fd].getUserNickName(), "PASS"));//pb car le nick name pas encore set
-    else if (users[fd].getIsConnected())
+  else if (users[fd].getIsConnected())
 		sendServerRpl(fd, ERR_ALREADYREGISTERED(users[fd].getUserNickName()));//pb car le nick name pas encore set
-    else if(msg.getParameters()[0] != _password)
+  else if(msg.getParameters()[0] != _password)
 		sendServerRpl(fd, ERR_PASSWDMISMATCH(users[fd].getUserNickName()));//pb car le nick name pas encore set
-    else if(msg.getParameters()[0] == _password)
-    {
-        users[fd].setIsConnected(true);
-    }
+  else if(msg.getParameters()[0] == _password)
+  {
+      users[fd].setIsConnected(true);
+  }
 }
